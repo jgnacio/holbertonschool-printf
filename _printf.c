0d804ef26ff70825690e400fd1861b7b744943eb
jgnacio <jgnaciogomez@gmail.com>
2022-11-07 13:26:56 -0800

    add a variable to count the lenght of full string.

--- _printf.c
+++ _printf.c
@@ -10,6 +10,7 @@ int _printf(const char *format, ...)
 	int i;
 	va_list arg_list;
 	int (*f)(va_list);
+	int lenght_full = 0;
 
 	va_start(arg_list, format);
 
@@ -18,12 +19,16 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			f = RCL(&format[i + 1]);
-			f(arg_list);
+			lenght_full += f(arg_list);
 			i++;
+
 		}
 		else
+		{
 			_putchar(format[i]);
+			lenght_full++;
+		}
 	}
 
-	return (i);
+	return (lenght_full);
 }
