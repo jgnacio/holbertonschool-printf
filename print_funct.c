531604e980083e19661038b001932d6a00536a79
jgnacio <jgnaciogomez@gmail.com>
2022-11-07 12:36:06 -0800

    add print_funct.c

---
+++ print_funct.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int print_srt(va_list arg)
+{
+	char *aux;
+	int i;
+
+	if (arg)
+	{
+		aux = va_arg(arg, char*);
+
+		for (i = 0; aux[i]; i++)
+		{
+			_putchar(aux[i]);
+		}
+		return (i);
+	}
+	return (-1);
+}
+
+/**
+ *
+ *
+ *
+ */
+int print_c(va_list arg)
+{
+	if (arg)
+	{
+		_putchar(va_arg(arg, int));
+		return (1);
+	}
+}
+/**
+ *
+ *
+ *
+ */
+int print_int(va_list arg)
+{
+	if (arg)
+	{
+		_putchar('I');
+		_putchar('N');
+		_putchar('T');
+		_putchar('\n');
+	}
+}
