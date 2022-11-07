6c332fed3c9a147ef8ee5256feddca8ddba3e26d
jgnacio <jgnaciogomez@gmail.com>
2022-11-07 12:28:04 -0800

    add main.h

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef struct typefunctions
+{
+	char identifier;
+	int (*function)(va_list);
+} t_func;
+
+int _printf(const char *format, ...);
+int (*RCL(const char *format))(va_list);
+int print_spaces(int count_spc);
+int print_c(va_list arg);
+int print_str(va_list arg);
+int print_int(va_list arg);
+void _putchar(char c);
+
+#endif
