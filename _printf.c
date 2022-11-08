5237b46a9021ca346627f56e2906a3164634622b
jgnacio <jgnaciogomez@gmail.com>
2022-11-08 06:25:18 -0800

    add the extra case for % and NULL, and fix return in both cases

--- _printf.c
+++ _printf.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
+ * _printf - function that produces output according to a format.
+ * @format: Is an string with the sentences or plain text to print.
  *
- *
- *
+ * Return: The lenght of all sentences and plain text.
  */
 int _printf(const char *format, ...)
 {
@@ -12,23 +13,39 @@ int _printf(const char *format, ...)
 	int (*f)(va_list);
 	int lenght_full = 0;
 
-	va_start(arg_list, format);
-
-	for (i = 0; format[i]; i++)
+	if (format)
 	{
-		if (format[i] == '%')
+		va_start(arg_list, format);
+		for (i = 0; format[i]; i++)
 		{
-			f = RCL(&format[i + 1]);
-			lenght_full += f(arg_list);
-			i++;
-
-		}
-		else
-		{
-			_putchar(format[i]);
-			lenght_full++;
+			if (format[i] == '%')
+			{
+				f = RCL(&format[i + 1]);
+				if (f == NULL)
+				{
+					if (format[i + 1] || format[i + 1] == '%')
+					{
+						_putchar(format[i]);
+						_putchar(format[i + 1]);
+						lenght_full += 2;
+						i++;
+						continue;
+					}
+					else
+						return (-1);
+				}
+				lenght_full += f(arg_list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				lenght_full++;
+			}
 		}
 	}
+	else
+		return (-1);
 
 	return (lenght_full);
 }
